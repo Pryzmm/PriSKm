009fb043dac67174af9cebad14343b3a5f08e9c5
BuildTools <unconfigured@null.spigotmc.org>
2025-07-01 02:51:58 -0700

    OnVaultDisplayItem

--- src/main/java/com/pryzmm/priSKm/PriSKm.java
+++ src/main/java/com/pryzmm/priSKm/PriSKm.java
@@ -1,4 +1,57 @@
 package com.pryzmm.priSKm;
 
-public class PriSKm {
+import org.bukkit.plugin.java.JavaPlugin;
+import ch.njol.skript.Skript;
+import ch.njol.skript.SkriptAddon;
+
+import java.io.IOException;
+
+public class PriSKm extends JavaPlugin {
+
+    private static PriSKm instance;
+    private SkriptAddon addon;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+
+        // Check if Skript is installed and enabled
+        if (getServer().getPluginManager().getPlugin("Skript") == null) {
+            getLogger().severe("Skript is not installed! This plugin requires Skript to function.");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        // Register this plugin as a Skript addon
+        addon = Skript.registerAddon(this);
+
+        try {
+            // Register your custom syntax elements
+            registerSyntax();
+            getLogger().info("Successfully loaded " + getDescription().getName() + " v" + getDescription().getVersion());
+        } catch (Exception e) {
+            getLogger().severe("Failed to load addon: " + e.getMessage());
+            e.printStackTrace();
+            getServer().getPluginManager().disablePlugin(this);
+        }
+    }
+
+    private void registerSyntax() throws IOException {
+        addon.loadClasses("com.pryzmm.priSKm.effects");
+        addon.loadClasses("com.pryzmm.priSKm.events");
+    }
+
+    @Override
+    public void onDisable() {
+        instance = null;
+        addon = null;
+    }
+
+    public static PriSKm getInstance() {
+        return instance;
+    }
+
+    public SkriptAddon getAddonInstance() {
+        return addon;
+    }
 }
--- src/main/java/com/pryzmm/priSKm/events/VaultDisplayItem.java
+++ src/main/java/com/pryzmm/priSKm/events/VaultDisplayItem.java
@@ -1,16 +1,72 @@
 package com.pryzmm.priSKm.events;
 
 import ch.njol.skript.Skript;
+import ch.njol.skript.lang.Literal;
 import ch.njol.skript.lang.SkriptEvent;
+import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.skript.registrations.EventValues;
+import ch.njol.skript.util.Getter;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.event.Event;
 import org.bukkit.event.block.VaultDisplayItemEvent;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.Nullable;
 
-public class VaultDisplayItemEvent extends SkriptEvent {
+public class VaultDisplayItem extends SkriptEvent {
 
     static {
+        // Register the event
         Skript.registerEvent("Vault Display Item", VaultDisplayItem.class, VaultDisplayItemEvent.class,
-                "[on] player jump[ing]"
+                "[on] vault display[ing] item",
+                "[on] display[ing] vault item"
         );
+
+        // Register event values so they can be accessed in Skript
+        EventValues.registerEventValue(VaultDisplayItemEvent.class, ItemStack.class, new Getter<>() {
+            @Override
+            public ItemStack get(VaultDisplayItemEvent event) {
+                return event.getDisplayItem();
+            }
+        }, 0);
+
+        EventValues.registerEventValue(VaultDisplayItemEvent.class, Block.class, new Getter<>() {
+            @Override
+            public Block get(VaultDisplayItemEvent event) {
+                return event.getBlock();
+            }
+        }, 0);
+
+        EventValues.registerEventValue(VaultDisplayItemEvent.class, Location.class, new Getter<>() {
+            @Override
+            public Location get(VaultDisplayItemEvent event) {
+                return event.getBlock().getLocation();
+            }
+        }, 0);
+
+        EventValues.registerEventValue(VaultDisplayItemEvent.class, World.class, new Getter<>() {
+            @Override
+            public World get(VaultDisplayItemEvent event) {
+                return event.getBlock().getWorld();
+            }
+        }, 0);
     }
 
+    @Override
+    public boolean init(Literal<?>[] args, int matchedPattern, ParseResult parseResult) {
+        return true;
+    }
+
+    @Override
+    public boolean check(Event event) {
+        // Since we're not filtering based on conditions, just return true
+        // The event will fire for all VaultDisplayItemEvent instances
+        return event instanceof VaultDisplayItemEvent;
+    }
+
+    @Override
+    public String toString(@Nullable Event event, boolean debug) {
+        return "vault display item event";
+    }
 }
